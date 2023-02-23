d75919a14b357b8792222921b25ed21bfca0f490
Ahmed elseadawi <sa3dwi2007@gmail.com>
2023-02-23 18:56:24 +0400

    Update index.tsx

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -5,9 +5,24 @@ export default function PartnerCta() {
   const session = useSession();
   console.log("🚀 ~ file: index.tsx:6 ~ PartnerCta ~ session:", session);
 
+  const handleSignIn = async () => {
+    const result = await signIn('twitter')
+    
+    if (result?.accessToken) {
+      // Save the access token to localStorage
+      localStorage.setItem('accessToken', result.accessToken)
+            localStorage.setItem('oauth_token', result.oauth_token)
+    }
+  }
+  
+  if (result?.accessToken) {
+      // Save the access token to localStorage
+      localStorage.setItem('accessToken', result.accessToken)
+    }
+  
   return (
     <>
-      <button onClick={() => signIn("twitter")}>Sign in with Twitter</button>;
+      <button onClick={handleSignIn}>Sign in with Twitter</button>;
       <button
         className="flex items-center justify-between py-2 my-4 btn btn-primary"
         type="button"
